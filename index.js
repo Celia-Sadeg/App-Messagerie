ddfbd79dc10c43364aac10e3f0d2b8ce92999889
celia sadeg <sadegcelia4@gmail.com>
2024-02-18 17:48:27 +0100

    test

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const io = require('socket.io')(server);
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`);
 });
+app.get('/test', (req, res) => {
+    console.log(`hello`);
+});
 
 server.listen(3000, () => {
     console.log("Listening on port 3000");
